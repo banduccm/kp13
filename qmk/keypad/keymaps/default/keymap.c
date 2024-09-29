a9f7ede074cb9c7d14ed3eeee9fbf80556986b14
Mike Banducci <mike@mbanducci.dev>
2024-09-29 15:26:46 -0500

    - Updated keymap and matrix for the default thumb pod  - Added a thumb pod plate design

--- qmk/keypad/keymaps/default/keymap.c
+++ qmk/keypad/keymaps/default/keymap.c
@@ -6,20 +6,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6,
         KC_7, KC_8, KC_9, KC_A, KC_B, KC_C, KC_D,
               KC_E, KC_F, KC_G, KC_H, KC_I,
-                    KC_J, KC_K, KC_L
+                    KC_J, KC_K, KC_L,
+                    KC_U, KC_L, KC_D, KC_R, KC_Z, KC_X, KC_Y, KC_V
     ),
     [1] = LAYOUT(   // gw2 layout
                                 DF(0), DF(2),
         KC_1, KC_2, KC_3, KC_W, KC_4, KC_5, KC_6,
         KC_7, KC_8, KC_A, KC_S, KC_D, KC_9, KC_0,
            KC_LCTL, KC_N, KC_H, KC_L, KC_GRAVE,
-                    KC_T, KC_J, KC_SPACE
+                    KC_T, KC_J, KC_SPACE,
+                    KC_U, KC_L, KC_D, KC_R, KC_Z, KC_X, KC_Y, KC_V
     ),
     [2] = LAYOUT(   // g13 default layout
                                 DF(1), DF(2),
         KC_1, KC_2, KC_Q, KC_W, KC_E, KC_R, KC_T,
         KC_3, KC_4, KC_A, KC_S, KC_D, KC_F, KC_G,
            KC_LSFT, KC_Z, KC_X, KC_C, KC_V,
-                KC_LCTL, KC_LALT, KC_SPACE
+                KC_LCTL, KC_LALT, KC_SPACE,
+                KC_U, KC_L, KC_D, KC_R, KC_Z, KC_X, KC_Y, KC_V
     ),
 };
